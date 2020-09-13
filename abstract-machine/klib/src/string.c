185cb2932694e4cbf7e6fe6a2ece9477e2239bf9
tracer-ics2020 <tracer@njuics.org>
2020-09-13 23:49:50 +0800

    >  run 181220076    Static hostname: zrz-laptop          Icon name: computer-laptop            Chassis: laptop         Machine ID: 372e7f05f2b343e5bce1789aaef2fd68            Boot ID: 96ebc6a1151a41a8acc1a494ee8d84fe   Operating System: Manjaro Linux             Kernel: Linux 5.7.19-2-MANJARO       Architecture: x86-64  23:49:51 up 11:25,  4 users,  load average: 0.59, 0.53, 0.58

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -16,8 +16,8 @@ char *strcpy(char* dst,const char* src) {
 
 char* strncpy(char* dst, const char* src, size_t n) {
   size_t i;
-  for (i = 0; i < n && src[i] != '\0'; i++) dst[i] = src[i];
-  for ( ; i < n; i++) dst[i] = '\0';
+  for (i = 0; i < n && src[i] != '/0'; i++) dst[i] = src[i];
+  for ( ; i < n; i++) dst[i] = '/0';
   if(*dst=='a') assert(strlen(dst)==2);
   return dst;
   /*char *p1=dst;
