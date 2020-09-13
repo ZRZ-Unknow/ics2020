f0491267a68d2bf5a066f6891871d168af31ff10
tracer-ics2020 <tracer@njuics.org>
2020-09-13 23:31:36 +0800

    >  compile 181220076    Static hostname: zrz-laptop          Icon name: computer-laptop            Chassis: laptop         Machine ID: 372e7f05f2b343e5bce1789aaef2fd68            Boot ID: 96ebc6a1151a41a8acc1a494ee8d84fe   Operating System: Manjaro Linux             Kernel: Linux 5.7.19-2-MANJARO       Architecture: x86-64  23:31:36 up 11:07,  3 users,  load average: 0.89, 0.74, 0.65

--- nemu/src/isa/x86/reg.c
+++ nemu/src/isa/x86/reg.c
@@ -63,5 +63,6 @@ word_t isa_reg_str2val(const char *s, bool *success) {
   for(int i=0;i<8;i++){
     if(strcmp(s,regsb[i])==0) return (i<4)?cpu.gpr[i]._8[0]:cpu.gpr[i]._8[1];
   }
+  *success=false;
   return 0;
 }
