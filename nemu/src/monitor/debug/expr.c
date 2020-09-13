ead3bdb9171ad8329a4aba98deb179b22030c2b7
tracer-ics2020 <tracer@njuics.org>
2020-09-13 15:12:42 +0800

    >  compile 181220076    Static hostname: zrz-laptop          Icon name: computer-laptop            Chassis: laptop         Machine ID: 372e7f05f2b343e5bce1789aaef2fd68            Boot ID: 96ebc6a1151a41a8acc1a494ee8d84fe   Operating System: Manjaro Linux             Kernel: Linux 5.7.19-2-MANJARO       Architecture: x86-64  15:12:43 up  2:48,  4 users,  load average: 0.34, 0.63, 0.60

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -87,23 +87,20 @@ static bool make_token(char *e) {
             i, rules[i].regex, position, substr_len, substr_len, substr_start);
 
         position += substr_len;
-
+        
         /* TODO: Now a new token is recognized with rules[i]. Add codes
          * to record the token in the array `tokens'. For certain types
          * of tokens, some extra actions should be performed.
          */
 
         switch (rules[i].token_type) {
-          case TK_NOTYPE : break;
-          default: {
-            tokens[nr_token].type=rules[i].token_type;
-            strncpy(tokens[nr_token].str,substr_start,substr_len);
-            nr_token++;
-          }
+          case TK_NOTYPE : continue;
           case '(' : {pare_check++;printf("(");break;}
           case ')' : {pare_check--;printf(")");break;}
         }
-
+        tokens[nr_token].type=rules[i].token_type;
+        strncpy(tokens[nr_token].str,substr_start,substr_len);
+        nr_token++;
         break;
       }
     }
